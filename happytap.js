8d2db2f25607e32b3958a388a5d3ee1ba43d44e6
mtsgi <oq@live.jp>
2019-11-02 15:26:28 +0900

    Initial Commit

---
+++ happytap.js
@@ -0,0 +1,66 @@
+class Game{
+    static init() {
+        Game.active = true;
+        $('#p1 h3').text(Game.p1);
+        $('#p2 h3').text(Game.p2);
+        Game.countdown();
+    }
+
+    static countdown() {
+        $('aside').text(Game.countremain);
+        Game.countremain -= 1;
+        if( Game.countremain < 0 ){
+            Game.start();
+        }
+        else setTimeout(Game.countdown, 1000);
+    }
+
+    static start() {
+        $('aside').hide();
+        $('#p1').off().on('touchstart mousedown', () => {
+            Game.p1 += 1;
+            if( Game.timeremain <= 2 ) $('#p1 h3').text('???');
+            else $('#p1 h3').text(Game.p1);
+        });
+        $('#p2').off().on('touchstart mousedown', () => {
+            Game.p2 += 1;
+            if( Game.timeremain <= 2 ) $('#p2 h3').text('???');
+            else $('#p2 h3').text(Game.p2);
+        });
+        Game.timecount();
+    }
+
+    static timecount() {
+        $('header').text(Game.timeremain);
+        Game.timeremain -= 1;
+        if( Game.timeremain == 2 ){
+            $('#p1 h3').text('???');
+            $('#p2 h3').text('???');
+        }
+        if( Game.timeremain < 0 ) Game.end();
+        else setTimeout(Game.timecount, 1000);
+    }
+
+    static end() {
+        alert('Finish');
+        $('#p1').off();
+        $('#p2').off();
+        $('#p1 h3').text(Game.p1);
+        $('#p2 h3').text(Game.p2);
+        $('aside').show().text('Result (Click to Restart)');
+        Game.countremain = 3;
+        Game.timeremain = 10;
+        Game.p1 = 0;
+        Game.p2 = 0;
+        Game.active = false;
+    }
+}
+
+Game.countremain = 3;
+Game.timeremain = 10;
+Game.p1 = 0;
+Game.p2 = 0;
+
+$('main').on('touchstart mousedown', () => {
+    if( !Game.active ) Game.init();
+});
