5ae0e161990164a561e623b6f530b2ede42a7ef4
mtsgi <oq@live.jp>
2019-11-02 15:47:23 +0900

    Buf Fix

--- happytap.js
+++ happytap.js
@@ -17,12 +17,12 @@ class Game{
 
     static start() {
         $('aside').hide();
-        $('#p1').off().on('touchstart mousedown', () => {
+        $('#p1').off().on(Game.event, () => {
             Game.p1 += 1;
             if( Game.timeremain <= 2 ) $('#p1 h3').text('???');
             else $('#p1 h3').text(Game.p1);
         });
-        $('#p2').off().on('touchstart mousedown', () => {
+        $('#p2').off().on(Game.event, () => {
             Game.p2 += 1;
             if( Game.timeremain <= 2 ) $('#p2 h3').text('???');
             else $('#p2 h3').text(Game.p2);
@@ -60,7 +60,19 @@ Game.countremain = 3;
 Game.timeremain = 10;
 Game.p1 = 0;
 Game.p2 = 0;
+Game.event = null;
 
-$('main').on('touchstart mousedown', () => {
-    if( !Game.active ) Game.init();
+$('main').on('touchstart', () => {
+    if( !Game.active ){
+        Game.event = 'touchstart';
+        Game.init();
+    }
+});
+
+
+$('main').on('mousedown', () => {
+    if( !Game.active ){
+        Game.event = 'mousedown';
+        Game.init();
+    }
 });
